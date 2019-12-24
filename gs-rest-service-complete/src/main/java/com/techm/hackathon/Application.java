b85f6d744fb59904774b3c283c7799262a8aa2b2
am827x <am827x@us.att.com>
2019-12-24 12:26:01 +0530

    hackathon code changes

--- gs-rest-service-complete/src/main/java/com/techm/hackathon/Application.java
+++ gs-rest-service-complete/src/main/java/com/techm/hackathon/Application.java
@@ -2,13 +2,18 @@ package com.techm.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//@ComponentScan("com.techm.hackathon.employee")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 }
